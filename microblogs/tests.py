6b6bd8f5df86c537ed7cb50a448fa736f996941a
Eduard Ragea <eduard.ragea@kcl.ac.uk>
2021-10-16 17:03:24 +0300

    Enforce input validation on User model

--- microblogs/tests.py
+++ microblogs/tests.py
@@ -1,3 +1,186 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            '@johndoe',
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.org',
+            password='Password123',
+            bio='Hi, my name is John.'
+        )
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+
+    def test_username_cannot_be_blank(self):
+        self.user.username = ''
+        self._assert_user_is_invalid()
+
+    def test_username_can_be_30_charcaters_long(self):
+        self.user.username = '@' + 'x' * 29
+        self._assert_user_is_valid()
+
+    def test_username_cannot_be_30_charcaters_long(self):
+        self.user.username = '@' + 'x' * 30
+        self._assert_user_is_invalid()
+
+    def test_username_must_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.username = second_user.username
+        self._assert_user_is_invalid()
+
+    def test_username_must_start_with_at_symbol(self):
+        self.user.username = 'johndoe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_only_alphanumericals_after_at(self):
+        self.user.username = '@john!doe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_at_least_3_alphanumericals_after_at(self):
+        self.user.username = '@jo'
+        self._assert_user_is_invalid()
+
+    def test_username_may_contain_numbers(self):
+        self.user.username = '@j0hndoe2'
+        self._assert_user_is_valid()
+
+    def test_username_must_contain_only_one_at(self):
+        self.user.username = '@@johndoe'
+        self._assert_user_is_invalid()
+
+    def test_first_name_need_not_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.first_name = second_user.first_name
+        self._assert_user_is_valid()
+
+    def test_first_name_must_not_be_blank(self):
+        self.user.first_name = ''
+        self._assert_user_is_invalid()
+
+    def test_first_name_may_contain_50_characters(self):
+        self.user.first_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_first_name_must_not_contain_more_than_50_characters(self):
+        self.user.first_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_last_name_need_not_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.last_name = second_user.last_name
+        self._assert_user_is_valid()
+
+    def test_last_name_must_not_be_blank(self):
+        self.user.last_name = ''
+        self._assert_user_is_invalid()
+
+    def test_last_name_may_contain_50_characters(self):
+        self.user.last_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_last_name_must_not_contain_more_than_50_characters(self):
+        self.user.last_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+    def test_email_must_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.email = second_user.email
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_be_blank(self):
+        self.user.email = ''
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_username(self):
+        self.user.email = '@example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_at_symbol(self):
+        self.user.email = 'johndoe.example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain_name(self):
+        self.user.email = 'johndoe@.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain(self):
+        self.user.email = 'johndoe@example'
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_contain_more_than_one_at(self):
+        self.user.email = 'johndoe@@example.org'
+        self._assert_user_is_invalid()
+
+    def test_bio_need_not_be_unique(self):
+        second_user = self._create_second_user()
+        self.user.bio = second_user.bio
+        self._assert_user_is_valid()
+
+    def test_bio_may_be_blank(self):
+        self.user.bio = ''
+        self._assert_user_is_valid()
+
+    def test_bio_may_contain_520_characters(self):
+        self.user.bio = 'x' * 520
+        self._assert_user_is_valid()
+
+    def test_bio_must_not_contain_more_than_520_characters(self):
+        self.user.bio = 'x' * 521
+        self._assert_user_is_invalid()
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should be valid')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
+
+    def _create_second_user(self):
+        user = User.objects.create_user(
+            '@janedoe',
+            first_name='Jane',
+            last_name='Doe',
+            email='janedoe@example.org',
+            password='Password123',
+            bio='Hi, my name is Jane.'
+        )
+        return user
+
+"""def factorial(integer):
+    if integer<0:
+        raise ValueError(f'factorial requires a non-negative integer, got {integer}')
+    result = 1
+    for i in range(2, integer+1):
+        result *= i
+    return result
+
+class UnitTestCase(TestCase):
+    def test_factorial_of_0(self):
+        self.assertEqual(1, factorial(0))
+
+    def test_factorial_of_1(self):
+        self.assertEqual(1, factorial(1))
+
+    def test_factorial_of_2(self):
+        self.assertEqual(2, factorial(2))
+
+    def test_factorial_of_3(self):
+        self.assertEqual(6, factorial(3))
+
+    def test_factorial_of_5(self):
+        self.assertEqual(120, factorial(5))
+
+    def test_factorial_of_10(self):
+        self.assertEqual(3628800, factorial(10))
+
+    def test_factorial_of_minus_1(self):
+        with self.assertRaises(ValueError):
+            factorial(-1)"""
